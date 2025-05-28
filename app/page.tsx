a22ad06e0d5456bf0d83c65ab3763c3e655a2bd7
JayHaroo <saturnomjey09@gmail.com>
2025-05-28 16:54:54 +0800

    Refactor Home component to enhance social media icon functionality and fix minor formatting issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   Card,
@@ -20,7 +21,7 @@ export default function Home() {
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Card className="w-full w-[300px] h-auto items-center justify-center">
+        <Card className="w-[300px] h-auto items-center justify-center">
           <CardHeader className="flex flex-col items-center justify-center space-y-4 mb-2 w-full">
             <Avatar className="w-[120px] h-[120px]">
               <AvatarImage src="./dp.jpeg" alt="Avatar Image" className="object-cover"/>
@@ -28,7 +29,7 @@ export default function Home() {
             </Avatar>
             
             <CardTitle>Meguri.kenn</CardTitle>
-            <CardDescription>Hello! I like to draw :> </CardDescription>
+            <CardDescription>Hello! I like to draw :{'>'} </CardDescription>
             <CardDescription>meguri.kenn@gmail.com</CardDescription>
             
           </CardHeader>
@@ -38,8 +39,13 @@ export default function Home() {
           <CardFooter className="flex flex-col items-center justify-center space-y-4">
             <p>Connect with me!</p>
             <div className="grid grid-cols-4 gap-2">
-              <FaFacebook color="white" size={30}/>
-              <FaInstagram color="white" size={30}/>
+              <FaFacebook color="white" size={30}
+                onClick={() => openUrl("https://www.facebook.com/profile.php?id=100082973132783")}/>
+
+              <FaInstagram color="white" size={30}
+                onClick={() => openUrl("https://www.instagram.com/meguri.ken/")}
+              />
+
               <FaXTwitter color="white" size={30}/>
               <FaPixiv color="white" size={30}/>
               <FaTiktok color="white" size={30}/>
