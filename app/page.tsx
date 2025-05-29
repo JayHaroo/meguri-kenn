2c08cde506b14c8b0158d91547bd33b5341a073d
JayHaroo <saturnomjey09@gmail.com>
2025-05-29 11:04:28 +0800

    Add email display and improve social media icon accessibility in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   Card,
@@ -12,63 +12,96 @@ import {
 import { FaFacebook, FaInstagram } from "react-icons/fa";
 import { FaXTwitter, FaPixiv, FaTiktok, FaBluesky } from "react-icons/fa6";
 import { SiKofi } from "react-icons/si";
+import { MdEmail } from "react-icons/md";
 
 export default function Home() {
-
   const openUrl = (url: string) => {
     window.open(url, "_blank");
   };
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Card className="w-[300px] h-auto items-center justify-center">
-          <CardHeader className="flex flex-col items-center justify-center space-y-4 mb-2 w-full">
-            <Avatar className="w-[120px] h-[120px]">
-              <AvatarImage src="./dp.jpeg" alt="Avatar Image" className="object-cover"/>
-              <AvatarFallback>AB</AvatarFallback>
-            </Avatar>
-            
-            <CardTitle>Meguri.kenn</CardTitle>
-            <CardDescription>Hello! I like to draw :{'>'} </CardDescription>
-            <CardDescription>meguri.kenn@gmail.com</CardDescription>
-            
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter className="flex flex-col items-center justify-center space-y-4">
-            <p>Connect with me!</p>
-            <div className="grid grid-cols-4 gap-2">
-              <FaFacebook color="white" size={30}
-                onClick={() => openUrl("https://www.facebook.com/profile.php?id=100082973132783")}
-                title="Facebook"/>
+      <Card className="w-[300px] h-auto items-center justify-center">
+        <CardHeader className="flex flex-col items-center justify-center space-y-4 mb-2 w-full">
+          <Avatar className="w-[120px] h-[120px]">
+            <AvatarImage
+              src="./dp.jpeg"
+              alt="Avatar Image"
+              className="object-cover"
+            />
+            <AvatarFallback>AB</AvatarFallback>
+          </Avatar>
 
-              <FaInstagram color="white" size={30}
-                onClick={() => openUrl("https://www.instagram.com/meguri.ken/")}
-                title="Instagram"/>
+          <CardTitle>Meguri.kenn</CardTitle>
+          <CardDescription>Hello! I like to draw :{">"} </CardDescription>
+          <div className="flex">
+            <MdEmail color="white" size={20} className="mr-1" />
+            <CardDescription className="text-white">meguri.kenn@gmail.com</CardDescription>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter className="flex flex-col items-center justify-center space-y-4">
+          <p>Connect with me!</p>
+          <div className="grid grid-cols-4 gap-2">
+            <FaFacebook
+              color="white"
+              size={30}
+              onClick={() =>
+                openUrl(
+                  "https://www.facebook.com/profile.php?id=100082973132783"
+                )
+              }
+              title="Facebook"
+            />
 
-              <FaXTwitter color="white" size={30}
-               onClick={() => openUrl("https://x.com/meguri_kenn")}
-               title="X"/>
+            <FaInstagram
+              color="white"
+              size={30}
+              onClick={() => openUrl("https://www.instagram.com/meguri.ken/")}
+              title="Instagram"
+            />
 
-              <FaPixiv color="white" size={30}
-               onClick={() => openUrl("https://www.pixiv.net/en/users/76892551")}
-               title="Pixiv"/>
+            <FaXTwitter
+              color="white"
+              size={30}
+              onClick={() => openUrl("https://x.com/meguri_kenn")}
+              title="X"
+            />
 
-              <FaTiktok color="white" size={30}
-               onClick={() => openUrl("https://www.tiktok.com/@meguri.art")}
-               title="TikTok"/>
+            <FaPixiv
+              color="white"
+              size={30}
+              onClick={() => openUrl("https://www.pixiv.net/en/users/76892551")}
+              title="Pixiv"
+            />
 
-              <FaBluesky color="white" size={30}
-               onClick={() => openUrl("https://bsky.app/profile/meguri-kenn.bsky.social")}
-               title="Bluesky"/>
+            <FaTiktok
+              color="white"
+              size={30}
+              onClick={() => openUrl("https://www.tiktok.com/@meguri.art")}
+              title="TikTok"
+            />
 
-              <SiKofi color="white" size={30}
-               onClick={() => openUrl("https://ko-fi.com/meguriken")}
-               title="Ko-Fi"/>
-            </div>
-          </CardFooter>
-        </Card>
+            <FaBluesky
+              color="white"
+              size={30}
+              onClick={() =>
+                openUrl("https://bsky.app/profile/meguri-kenn.bsky.social")
+              }
+              title="Bluesky"
+            />
+
+            <SiKofi
+              color="white"
+              size={30}
+              onClick={() => openUrl("https://ko-fi.com/meguriken")}
+              title="Ko-Fi"
+            />
+          </div>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
